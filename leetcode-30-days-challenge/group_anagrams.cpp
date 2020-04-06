fbaba3aa22a7cee77d997e7251118a43a0d2cb99
Sahil Nishal <snishal33@gmail.com>
2020-04-06 14:57:09 +0530

    Day 6 optional solutions

--- leetcode-30-days-challenge/group_anagrams.cpp
+++ leetcode-30-days-challenge/group_anagrams.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -26,6 +27,66 @@ public:
         }
         groups.emplace_back(group);
         group.clear();
+        
+        return groups;
+    }
+};
+
+// Solution 2
+class Solution {
+private:
+    string string_to_key(const string& s){
+        vector<int> count(26);
+        
+        for(const char& ch : s){
+            count[ch - 'a']++;
+        }
+        
+        string key = "";
+        for(int i = 0; i < 26; i++){
+            key += to_string(count[i]) + "#";
+        }
+        return key;
+    }
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if(strs.empty()){
+            return {};
+        }
+        unordered_map<string, vector<string>>map;
+        
+        for(const string& s : strs){
+            map[string_to_key(s)].emplace_back(s);
+        }
+        
+        vector<vector<string>> groups;
+        for(const auto& pair : map){
+            groups.emplace_back(pair.second);
+        }
+        
+        return groups;
+    }
+};
+
+// Solution 3
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if(strs.empty()){
+            return {};
+        }
+        unordered_map<string, vector<string>>map;
+        
+        for(const string& s : strs){
+            string s_copy = s;
+            sort(s_copy.begin(), s_copy.end());
+            map[s_copy].emplace_back(s);
+        }
+        
+        vector<vector<string>> groups;
+        for(const auto& pair : map){
+            groups.emplace_back(pair.second);
+        }
         
         return groups;
     }
